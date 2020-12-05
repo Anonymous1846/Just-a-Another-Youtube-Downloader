e05d5283bf26889e55231677e563e4c0f6dd90fd
Sharath Sunil <alphatrinity17@gmail.com>
2020-12-05 22:20:53 +0530

    Comments Added Refactoring and Encapsulation for Easier Resuse

--- Downloader.py
+++ Downloader.py
@@ -1,6 +1,7 @@
 #The Python file has two functions namely for downloading the Single Video and For Downloading the Playlist
-
+#The functions defined in this module will be called there !
 def single_video():
+
     pass
 def playlist_download():
     pass
