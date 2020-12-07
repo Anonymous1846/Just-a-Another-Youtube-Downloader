8dad3e38eb78147a687754956080e063fb32d938
Sharath Sunil <alphatrinity17@gmail.com>
2020-12-07 19:07:49 +0530

    The Downloader File Changed Threding included for faster Download in Playlists

--- Downloader.py
+++ Downloader.py
@@ -1,16 +1,19 @@
-#The Python file has two functions namely for downloading the Single Video and For Downloading the Playlist
-#The functions defined in this module will be called there !
-#Library for Downloading the Videos
+# The Python file has two functions namely for downloading the Single Video and For Downloading the Playlist
+# The functions defined in this module will be called there !
+# Library for Downloading the Videos
 import pytube
-#For Regular Expressions
+# For Regular Expressions
 import re
-#Time Module Imported to display the Download Time !
+# Time Module Imported to display the Download Time !
 import time
+import threading
+
+
 class Downloader():
     def __init__(self):
         self.dic_for_video = {}
 
-    def download_single_video(self,Output_Path,video_link):
+    def download_single_video(self, Output_Path, video_link):
         try:
             # Fetching the Video Using the Youtube Object From the link provided by the user !
             y = pytube.YouTube(video_link)
@@ -44,26 +47,18 @@ class Downloader():
             print(f"Oops An Error Occured While Traversing the Link,Looks Like the Video URL is Corrupted !")
         finally:
             print('Done....')
-    def download_playlist(self,Output_Path,playlist_link):
+
+    def download_playlist(self, Output_Path, playlist_link):
         # Object of The Playlist Class
-        playlist=pytube.Playlist(playlist_link)
+        initial = time.time()
+        playlist = pytube.Playlist(playlist_link)
         print(f'Video Title: {playlist.title} Number of Videos: {len(playlist.video_urls)}')
-        try:
-            video=pytube.YouTube(playlist.video_urls.__getitem__(0))
-            counter = 1
-            format_value = input('1)video\n2)audio')
-            for i in video.streams.filter(type='video' if format_value == '1' else 'audio'):
-                # used to show the Type and Quality which are seperated by Spaces
-                list = str(i).split(" ")
-                itag, type, quality, fps_bitrate, audio_video = re.findall(r'"([^"]*)"',
-                                                                           str(list[1]) + str(list[2]) + str(
-                                                                               list[3]) + str(
-                                                                               list[4]) + str(list[6]))
-                # The Integer is mapped to an unique iTag so that user can choose easily
-                self.dic_for_video[counter] = itag
-                counter=counter+1
-                id_number = input('Enter The Id Number of The Video To Download The Video !')
-                for video in playlist.videos:
-                    video.streams.get_by_itag(self.dic_for_video.get(id_number)).download(Output_Path)
-        except Exception as exp:
-            print('Something Went Wrong !')
+        for video in playlist.video_urls:
+            threading.Thread(target=self.download_playlist_video(Output_Path, video)).start()
+        print(f'Playlist Download Complete in {time.time() - initial} seconds')
+
+    def download_playlist_video(self, Output_Path, video_url):
+        initial = time.time()
+        video = pytube.YouTube(video_url)
+        video.streams.filter(progressive=True).get_highest_resolution().download(Output_Path)
+        print(f'{video.title} Downloaded in {time.time() - initial} seconds')
--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import os
 print(
     '*******************************Youtube Video Downloader****************************\nVersion 1.0'
 )
-
-option=int(input('1)Video\n2)Playlist'))
-download=Downloader()
-if option==1:
-    download.download_single_video()
-elif option==2:
-    download.download_playlist()
+Youtube_Video_Directory = 'Youtube Videos'
+Downloads = 'C:/Users/USER/Downloads/Youtube Videos'
+FINAL_PATH = os.path.join(Downloads, Youtube_Video_Directory)
+if os.path.isdir(FINAL_PATH):
+    print('Path Already Exists !')
+else:
+    os.mkdir(FINAL_PATH)
+option = int(input('1)Video\n2)Playlist'))
+download = Downloader()
+if option == 1:
+    video_link = input('Paste Video Link: ')
+    download.download_single_video(FINAL_PATH, video_link)
+elif option == 2:
+    playlist_link = input('Paste Playlist Link: ')
+    download.download_playlist(FINAL_PATH,playlist_link)
